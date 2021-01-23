748a5722dc83dfc47078d8524125fac5e779561d
Jannis Spiekermann <janniss@hotmail.de>
2021-01-23 01:15:42 +0100

    Data Reading
    I have made use of a nested Data Structure:
    LanguageData -> Problem (-> Unknown)
    -> Candidate -> Text

--- scripts/train.py
+++ scripts/train.py
@@ -7,6 +7,10 @@ from sklearn.svm import SVC
 
 """
 
+from read_data import LanguageData
+from read_data import LANGUAGES
+from read_data import PROBLEM_IDS
+
 
 def train(train_feature_vector, train_candidate_vector):
     pass
@@ -17,6 +21,24 @@ def test(test_feature_vector, test_candidate_vector):
 
 
 if __name__ == "__main__":
-    # Here goes the whole pipeline of data reading, text processing, feature 
-    # selection, training and evaluating
-    pass
+    """
+    Here goes the whole pipeline of data reading, text processing, feature
+    selection, training and evaluating
+    """
+
+    # Data Reading - Three language objects are set up that carry all texts
+    # split up into problems -> candidates -> texts
+    language_data_objs = [
+        LanguageData(language, id_list)
+        for num, (language, id_list) in enumerate(zip(LANGUAGES, PROBLEM_IDS))
+    ]
+    # NOTE: Look in read_data.py how to access these objects.
+    english, french, italian, spanish = language_data_objs
+
+    # Text Processing goes here.
+
+    # Feature Selection and Feature Setup Goes Here
+
+    # Train and Test
+
+    # Evaluate
