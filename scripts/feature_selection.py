c88845c7412bb1bfb87aa05932b04ea7227e8db9
Jannis Spiekermann <janniss@hotmail.de>
2021-01-27 15:40:46 +0100

    Improve description in feature_selection.py

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -3,18 +3,38 @@ Here we should implement multiple functions for feature selection.
 
 Below are some examples of functions that care about single features.
 """
+from collections import Counter
+import text_processing
+import numpy as np
 
 
-def extract_word_counts():
+def extract_word_counts(text,lemm=True,lang="english"):
     """
     This function requires tokenization and lemmatization.
-    :return: This should return a numpy array.
+    text : input text
+    lemm : if lemm is True, do the lemmatization.
+    lang : language of the sentence. Default language is English
+    :return: word_count, count of each word as a dictionary
     """
-    pass
+    ### Use the function from text_processing
+    # tokenization
+    word_list = text_processing.tokenize_text(text)
+
+    # if lemm == True, do the lemmatization
+    if lemm:
+        word_list = text_processing.lemmatize_words(word_list, lang)
+
+    # word count
+    word_count = Counter(word_list)
+    print(type(word_count))
+
+    return word_count
 
 
 def extract_sentence_lengths(text):
     """
+    -BLOCKED-
+
     This function requires tokenization and lemmatization.
     This function also requires a sentence splitting method.
 
@@ -31,6 +51,8 @@ def extract_sentence_lengths(text):
 
 def convert_word_counts():
     """
+    -BLOCKED-
+
     Find a way to convert the word counts of a text into a meaningful
     numeric representation that can be used as input to the SVM.
 
@@ -46,6 +68,8 @@ def convert_word_counts():
 
 def convert_sentence_lengths():
     """
+    -BLOCKED-
+
     Find a way to convert the sentence lengths of a text into a meaningful
     numeric representation that can be used as input to the SVM.
 
@@ -62,24 +86,35 @@ def combine_features_per_text():
     This function should combine the arrays produced by the conversion
     functions above into one single numpy array (representing one text).
 
-    :return: This should return a numpy array.
+    :return: This should return a numpy array of size (1 * num_total_features)
     """
     pass
 
 
 def combine_features_all_texts():
     """
+    The function will be used twice, once for our training data and
+    once for our testing data.
+
     This function should combine all features of all texts into one
     big numpy array of the shape:
 
-    num_texts * num_total_features
+    (num_texts * num_total_features)
 
     Also, the function should return the numpy array of candidates,
     which will be our labels.
     Shape should be:
 
-    num_texts * 1
+    (num_texts * 1)
 
     :return: a Tuple of numpy arrays -> (features, labels)
     """
     pass
+
+
+if __name__ == "__main__":
+
+    # Testing extract_word_counts
+    text = "I can't help but think that if you were still alive you would have solved it in minutes in the U.S.A., and the victim would still be alive. The bananas cost $4.5, which 77.8% of what apples cost."
+    print(extract_word_counts(text))
+    print(extract_word_counts(text, lemm=False))
